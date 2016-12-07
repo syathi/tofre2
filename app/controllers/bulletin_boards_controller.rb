64c2771ab5a9487aa759130cc1c632a20ea6ad65
teikyo376 <teikyo376@gmail.com>
2016-12-07 10:50:44 +0900

    moja8

--- app/controllers/bulletin_boards_controller.rb
+++ app/controllers/bulletin_boards_controller.rb
@@ -1,7 +1,7 @@
 class BulletinBoardsController < ApplicationController
 
     def index
-        #賞味期限が１週間以内に迫ってるやーつと買ってから1習慣経ってるやーつ
+        #賞味期限が１週間以内に迫ってるやーつと買ってから1週間経ってるやーつ
         @public_foods = Refrigerator.where.not(deadline: nil).where(release: 1)
         @public_fresh= Refrigerator.where(deadline: nil).where(release: 1)
         @private_foods = Refrigerator.where.not(deadline: nil).where(release: 1)
